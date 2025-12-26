b57dec8a1fff2142b11b7551a9e38d4092091f6e
leopii98 <rippergames98@gmail.com>
2025-12-26 18:52:12 +0100

    fix-main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,22 @@ import comics from "./data/comics";
 function Main() {
     return (
         <header id="content-here" className="bg-black text-white text-center py-5">
-            <div>
-                <img id="bg-jumbotron" src="/jumbotron.jpg" alt="jumbotronimg" />
-                <div>
+            <div id="bg-jumbotron"><img id="bg-jumbotron" src="/jumbotron.jpg" alt="jumbotronimg" />
+                <div class="current-series-label mb-5">
+                    Current Series
+                </div>
+            </div>
+            <div className="container py-5">
+                <div className="row">
                     {comics.map(comics => {
-                        return <div><img src={comics.thumb} alt={comics.series} />
+                        return <div className="col-2 comic-card"><img src={comics.thumb} alt={comics.series} />
                             <h5>{comics.series}</h5>
                         </div>
                     })}
                 </div>
+            </div>
+            <div class="text-center mt-5">
+                <button class="btn btn-load-more">Load More</button>
             </div>
         </header >
     );
