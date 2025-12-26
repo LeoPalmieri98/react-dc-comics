40b5c3db14638723036254da74300f8d7cec4b59
leopii98 <rippergames98@gmail.com>
2025-12-26 18:18:45 +0100

    add-main&cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
+import comics from "./data/comics";
 function Main() {
     return (
         <header id="content-here" className="bg-black text-white text-center py-5">
-            <h3>Content goes Here...</h3>
-        </header>
+            <div>
+                <img id="bg-jumbotron" src="/jumbotron.jpg" alt="jumbotronimg" />
+                <div>
+                    {comics.map(comics => {
+                        return <div><img src={comics.thumb} alt={comics.series} />
+                            <h5>{comics.series}</h5>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </header >
     );
 }
 
